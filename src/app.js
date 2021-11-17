3d065c3554fb69632a075307d86be5862e8f00e0
Francesca <93705589+FranSanc@users.noreply.github.com>
2021-11-17 07:25:49 +0000

    Added portion of the forecast HTML to JS

--- src/app.js
+++ src/app.js
@@ -21,6 +21,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function dispayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="forecast-day">${day}</div>
+      <div class="icon">🌧️</div>
+      <div class="forecast-temperature">
+        <span class="forecast-temperature-max">18°</span>
+        <span class="forecast-temperature-min">12°</span>
+      </div>
+    </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -84,3 +107,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("London");
+dispayForecast();
